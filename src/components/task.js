06e24b4992bf7de44a3ba89120b5df6bdd29d1fd
Sanjay Kanth <sanjaykanth45@gmail.com>
2024-10-28 09:59:17 +0530

    files added

---
+++ src/components/task.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './task.css';
+
+function Task(){
+   
+    return(
+      <>
+     <h1 >YOU CAN<br></br>
+        HIRE FREELANCER<br></br>HERE</h1>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page </p>
+        <button style={{color:'White',backgroundColor:'crimson',padding:'20px',width:'150px',fontSize:'1.2rem'}}>About Us</button>
+        <button style={{color:'black',backgroundColor:'white',padding:'20px',width:'150px',fontSize:'1.2rem',marginLeft:'10px'}}>Get A Quote</button>
+
+        </>
+    )
+}
+
+export default Task;
